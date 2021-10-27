955d534e9e899f3322c0b9cd363abc5318b91dda
Kirill Timchenko <49305219+adjsky@users.noreply.github.com>
2021-10-27 21:31:09 +0300

    Update v5

--- pages/index.tsx
+++ pages/index.tsx
@@ -21,7 +21,7 @@ function Home(): JSX.Element {
         uiState.setTheme(uiState.theme == "light" ? "dark" : "light")
       }}
     >
-      Change theme v4
+      Change theme v5
     </Button>
   )
 }
--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const version = "v0.4"
+const version = "v0.5"
 const cacheName = "very_cool_project-" + version
 
 const contentToCache = ["/"]
