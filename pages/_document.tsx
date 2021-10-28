476ff11ec92d7e8badbb6f7907ce75a2dadab2bb
Kirill Timchenko <49305219+adjsky@users.noreply.github.com>
2021-10-28 20:41:42 +0300

    Add manifest

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -43,6 +43,10 @@ class MyDocument extends Document {
             href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,300;0,400;0,700;1,300;1,400;1,700&display=swap"
             rel="stylesheet"
           />
+          <link
+            rel="manifest"
+            href="/manifest.json"
+          />
         </Head>
         <body>
           <Main />
--- pages/index.tsx
+++ pages/index.tsx
@@ -21,7 +21,7 @@ function Home(): JSX.Element {
         uiState.setTheme(uiState.theme == "light" ? "dark" : "light")
       }}
     >
-      Change theme v6
+      Change theme
     </Button>
   )
 }
